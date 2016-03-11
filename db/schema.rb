9063f25beae7196ed78b66af3e479243860932e1
Joe Ryan <joe@clanryan.us>
2016-03-11 14:23:44 -0500

    generate profiles model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311191634) do
+ActiveRecord::Schema.define(version: 20160311192249) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "gender"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "birth_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
