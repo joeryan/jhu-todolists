1fc7cca53dffa6a8c254cf23a8bb10bb19c33b4c
Joe Ryan <joe@clanryan.us>
2016-03-11 14:50:03 -0500

    generate scaffold for todoList

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311193047) do
+ActiveRecord::Schema.define(version: 20160311194922) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20160311193047) do
     t.boolean  "completed"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "list_name"
+    t.date     "list_due_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
